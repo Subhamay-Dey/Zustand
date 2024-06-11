6f77330755d3267b30f63f36ceb47f95e78b2c30
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-11 20:24:29 +0530

    sucessfully completed the first zustand progrram and heading to the higher program.

--- zustand-project1/src/App.tsx
+++ zustand-project1/src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Button } from './components/ui/button';
+import {create} from "zustand"
+
+const useStore = create<{
+  count: number;
+  inc: () => void;
+  dec: () => void;
+}>((set) => ({
+  count: 0,
+  inc: () => set((state) => ({count: state.count + 1})),
+  dec: () => set((state) => ({count: state.count - 1})),
+}))
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const store = useStore();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <>
+    <Button onClick={store.inc}>Increment</Button>
+    <h1>{store.count}</h1>
+    <Button onClick={store.dec}>Decrement</Button>
+  </>
   )
 }
 
